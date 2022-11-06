a32ea06d2cf1255277aaf181736207e946459e82
andreszapataq <andreszapataq@gmail.com>
2022-11-06 18:34:32 -0500

    logout function 2

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -53,6 +53,9 @@ export const authSlice = createSlice({
                 state.message = action.payload
                 state.user = null
             })
+            .addCase(logout.fulfilled, (state) => {
+                state.user = null
+            })
     }
 })
 
